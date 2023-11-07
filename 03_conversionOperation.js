5c9a60880325e600da51240f0a4c308374271207
KrishanSurela <surelakrishan60@gmail.com>
2023-11-07 14:18:27 +0530

    Conversion of datatypes

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -38,6 +38,61 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ************************* Opeartions **********
+
+let value =4
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); //power
+console.log(2/3);  
+console.log(2%3);  
+
+let str1 = "Hello"
+let str2 = " Krishan"
+let str3 =str1+str2
+
+console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+// console.log((3 + 4) * 5 % 3); //2
+// console.log(+true) // 1
+// console.log(+""); //0
+
+let num1, num2 , num3
+num1=num2=num3=3+4
+
+let  gameCounter =100
+++gameCounter
+console.log(gameCounter);
+
+//prefix increment => it return the value after increment 
+
+// postfix increment => it retun the value before increment
+
+
+
+
+
+
+
+console.log("1"+"2"); //12
+
+
+
+
+
+
+
+
+
+
 
 
 
