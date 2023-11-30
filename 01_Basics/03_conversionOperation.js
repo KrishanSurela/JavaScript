cee6149012bb3ab7b516f97218212a1d6ca018ab
KrishanSurela <surelakrishan60@gmail.com>
2023-11-30 16:30:54 +0530

    Basics Of JS

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,103 @@
+let score ="331bc"
+// let score =null
+// let score =undefined
+// let score = true
+// let score ="krishan"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber); //number
+
+console.log(valueInNumber);
+//if score is "33" then output will 33 as a number ("33"=>33)
+// if score is "3324n" as string then output will NaN or Not a Number ("3324n"=>NaN)
+
+// if score is null then output will 0. (null=>0)
+// if score is undefined then output will NaN(undefined => NaN)
+// if score is boolean then output will 0 or 1. (boolean=>0 or 1)
+// if score is "krishan" then output will NaN
+
+// number to boolean conversion
+let isLoggedIn=1;
+//let isLoggedIn =""
+//let isLoggedIn="Krishan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1  => true ;
+// 0  => false ; 
+// "" => false ; 
+// "Krishan" => true;
+
+// nuumbeer to string conversion
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ************************* Opeartions **********
+
+let value =4
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); //power
+console.log(2/3);  
+console.log(2%3);  
+
+let str1 = "Hello"
+let str2 = " Krishan"
+let str3 =str1+str2
+
+console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+// console.log((3 + 4) * 5 % 3); //2
+// console.log(+true) // 1
+// console.log(+""); //0
+
+let num1, num2 , num3
+num1=num2=num3=3+4
+
+let  gameCounter =100
+++gameCounter
+console.log(gameCounter);
+
+//prefix increment => it return the value after increment 
+
+// postfix increment => it retun the value before increment
+
+
+
+
+
+
+
+console.log("1"+"2"); //12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
