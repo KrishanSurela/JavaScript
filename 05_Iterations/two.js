e58d5dc12c16a7731c520a93ee4a10e25b257eb7
KrishanSurela <surelakrishan60@gmail.com>
2023-12-25 16:17:44 +0530

    do while and while loop in js

---
+++ 05_Iterations/two.js
@@ -0,0 +1,45 @@
+//while loop
+
+let index = 1
+while (index<=10) {
+    console.log(`Value of index is ${index}`);
+    index = index+2
+}
+
+let myArray =["shaktiman","krrish","naagraj","thor"]
+
+let arr =0
+
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+//do while loop
+let score =1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <=5);
+
+/*
+    Score is 1
+    Score is 2
+    Score is 3
+    Score is 4
+    Score is 5
+*/
+
+//interesting concept is here
+
+//if we initialize the variable out of the loop , do while loop run first then check the condition ,condition is false ,,there is no error
+
+//like
+
+let h =6
+do {
+    console.log(`Score is ${h}`); //6
+    h++
+} while (h <=5);
+
+
