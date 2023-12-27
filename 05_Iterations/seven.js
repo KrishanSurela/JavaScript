5a40239203a26f137a7afd6cb07bf0a4d55c1642
KrishanSurela <surelakrishan60@gmail.com>
2023-12-27 17:51:37 +0530

    DOM

--- 05_Iterations/seven.js
+++ 05_Iterations/seven.js
@@ -1,10 +1,14 @@
-const myNumbers = [1 ,2 ,3 ,4 ,5 ,6 ,7]
+const myNumbers = [1, 2, 3, 4, 5, 6, 7];
 
-const newNums = myNumbers.map( (num) => num+10)
+const newNums = myNumbers.map((num) => num + 10);
 
-const newNums1 = myNumbers.map( (num) => { return num+10})
+const newNums1 = myNumbers.map((num) => {
+  return num + 10;
+});
 
-myNumbers.forEach( (num) => { console.log(num+10);})
+myNumbers.forEach((num) => {
+  console.log(num + 10);
+});
 // 11
 // 12
 // 13
@@ -14,23 +18,21 @@ myNumbers.forEach( (num) => { console.log(num+10);})
 // 17
 
 console.log(newNums);
-    //   [
-    //     11, 12, 13, 14,
-    //     15, 16, 17
-    //   ]
+//   [
+//     11, 12, 13, 14,
+//     15, 16, 17
+//   ]
 console.log(newNums1);
 //   [
 //     11, 12, 13, 14,
 //     15, 16, 17
 //   ]
 
-//Interesting 
-
+//Interesting
 
 //###########  Channing  ###########
 
-
-const newNum = myNumbers.map((num) => num*10 ).map( (num)=> num+1)
+const newNum = myNumbers.map((num) => num * 10).map((num) => num + 1);
 
 console.log(newNum);
 
@@ -39,20 +41,21 @@ console.log(newNum);
 //     51, 61, 71
 //   ]
 
-const newNum1 = myNumbers.map((num) => num*10)
-                         .map((num)=> num+1)
-                         .filter((num)=> num>=40)
+const newNum1 = myNumbers
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num >= 40);
 
 console.log(newNum1); //[ 41, 51, 61, 71 ]
 
-//Reduce 
+//Reduce
 
-const myNumber = [1 ,2 ,3 ]
+const myNumber = [1, 2, 3];
 
-const myTotal = myNumber.reduce(function(acc,currval){
-    console.log(`acc:${acc} and currval :${currval}`);
-    return acc+currval
-},0)
+const myTotal = myNumber.reduce(function (acc, currval) {
+  console.log(`acc:${acc} and currval :${currval}`);
+  return acc + currval;
+}, 0);
 
 console.log(myTotal);
 
@@ -61,44 +64,42 @@ console.log(myTotal);
 // acc:3 and currval :3
 // 6
 
-const myTotal1 = myNumber.reduce( (acc,currval)=> acc+currval,0)
+const myTotal1 = myNumber.reduce((acc, currval) => acc + currval, 0);
 
-console.log(myTotal1)
+console.log(myTotal1);
 
-const myTotal2 = myNumber.reduce( (acc,currval)=> {
-    console.log(`acc:${acc} and currval :${currval}`);
-    return acc+currval
-},0)
+const myTotal2 = myNumber.reduce((acc, currval) => {
+  console.log(`acc:${acc} and currval :${currval}`);
+  return acc + currval;
+}, 0);
 
-console.log(myTotal2)
+console.log(myTotal2);
 
-//for project purpose 
+//for project purpose
 
-const shoppingCart =[
-    {
-        itemName: "JS-Course",
-        price: 800
-    },
-    {
-        itemName: "Py-Course",
-        price: 1500
-    },
-    {
-        itemName: "Java-Course",
-        price: 2500
-    },
-    {
-        itemName: "App Deve-Course",
-        price: 8000
-    },
-    {
-        itemName: "Web-Course",
-        price: 3800
-    }
-]
-   
-const priceToPay=shoppingCart.reduce((acc,item)=>(acc+item.price),0)
+const shoppingCart = [
+  {
+    itemName: "JS-Course",
+    price: 800,
+  },
+  {
+    itemName: "Py-Course",
+    price: 1500,
+  },
+  {
+    itemName: "Java-Course",
+    price: 2500,
+  },
+  {
+    itemName: "App Deve-Course",
+    price: 8000,
+  },
+  {
+    itemName: "Web-Course",
+    price: 3800,
+  },
+];
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
 
 console.log(priceToPay);
-
-
--- 05_Iterations/two.js
+++ 05_Iterations/two.js
@@ -1,26 +1,26 @@
 //while loop
 
-let index = 1
-while (index<=10) {
-    console.log(`Value of index is ${index}`);
-    index = index+2
+let index = 1;
+while (index <= 10) {
+  console.log(`Value of index is ${index}`);
+  index = index + 2;
 }
 
-let myArray =["shaktiman","krrish","naagraj","thor"]
+let myArray = ["shaktiman", "krrish", "naagraj", "thor"];
 
-let arr =0
+let arr = 0;
 
-while(arr < myArray.length){
-    console.log(`Value is ${myArray[arr]}`);
-    arr = arr+1
+while (arr < myArray.length) {
+  console.log(`Value is ${myArray[arr]}`);
+  arr = arr + 1;
 }
 
 //do while loop
-let score =1
+let score = 1;
 do {
-    console.log(`Score is ${score}`);
-    score++
-} while (score <=5);
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 5);
 
 /*
     Score is 1
@@ -36,10 +36,8 @@ do {
 
 //like
 
-let h =6
+let h = 6;
 do {
-    console.log(`Score is ${h}`); //6
-    h++
-} while (h <=5);
-
-
+  console.log(`Score is ${h}`); //6
+  h++;
+} while (h <= 5);
